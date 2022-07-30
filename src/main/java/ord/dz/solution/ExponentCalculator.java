448675775615e15bc3aa62f87d42475cc5a33133
anassvoronn <UQzbwQCt7R3uMfz>
2022-07-30 13:55:27 +0300

    ExponentCalculator

--- src/main/java/ord/dz/solution/ExponentCalculator.java
+++ src/main/java/ord/dz/solution/ExponentCalculator.java
@@ -1,12 +1,13 @@
 package ord.dz.solution;
 
 public class ExponentCalculator {
+
     public int getNumberPower(int number, int degree) {
         if (degree < 0) {
             throw new IllegalArgumentException("Multiplication does not occur");
         }
         int result = 1;
-        for (; degree !=  0; --degree){
+        for (; degree != 0; --degree) {
             result *= number;
 
         }
